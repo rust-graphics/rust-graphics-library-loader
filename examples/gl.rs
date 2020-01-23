c51f3c491956a4d15c3704770f3fbc703bee4f0b
Hossein Noroozpour <Hossein.Noroozpour@gmail.com>
2020-01-23 13:56:24 +0330

    gl example works on macos

--- examples/gl.rs
+++ examples/gl.rs
@@ -3,10 +3,12 @@ extern crate rust_graphics_library_loader as loader;
 fn main() {
     #[cfg(target_os = "windows")]
     let library_name = "opengl32.dll";
-    #[cfg(not(target_os = "windows"))]
+    #[cfg(target_os = "linux")]
     let library_name = "libGL.so";
+    #[cfg(target_os = "macos")]
+    let library_name = "/System/Library/Frameworks/OpenGL.framework/Libraries/libGL.dylib";
 
-    let linker = loader::Linker::new(library_name).expect("Can not find GL library.");
+    let linker = loader::Linker::new(library_name).expect(&format!("Can not find {} library.", library_name));
     let bind_vertex_array: Option<extern "C" fn(u32)> = linker.get_function("glBindVertexArray");
     if bind_vertex_array.is_some() {
         println!("bind_vertex_array found");
