18b31faaa985f7d78665583de2f1acf4a4958fe9
Hossein Noroozpour <Hossein.Noroozpour@gmail.com>
2020-01-23 14:03:47 +0330

    debug derive feature added

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ use rust_graphics_log::log_f;
 #[cfg(not(target_os = "windows"))]
 extern crate libc;
 
-#[derive(Debug)]
+#[cfg_attr(feature="debug_derive", derive(Debug))]
 pub struct Linker {
     #[cfg(not(target_os = "windows"))]
     link: *mut libc::c_void,
